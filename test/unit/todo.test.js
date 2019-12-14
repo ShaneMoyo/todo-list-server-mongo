7e15c41dd954b77a74a50c9e024515e30de6d569
Shane Moyo <smoyo@cvent.com>
2019-12-14 12:30:14 -0800

    Todo model tests now passing.

--- test/unit/todo.test.js
+++ test/unit/todo.test.js
@@ -9,17 +9,20 @@ describe('Todo model', () => {
     assert.equal(errors.user.kind, 'required');
     assert.equal(errors.date.kind, 'required');
     assert.equal(errors.status.kind, 'required');
+    assert.equal(errors.title.kind, 'required');
   });
 
   it('throws errors for incorrect data types', () => {
     const resource = new Todo({
       user: {},
       date: {},
-      status: {}
+      status: {},
+      title: {}
     });
     const { errors } = resource.validateSync();
     assert.equal(errors.user.kind, 'ObjectID');
     assert.equal(errors.date.kind, 'Date');
     assert.equal(errors.status.kind, 'String');
+    assert.equal(errors.title.kind, 'String');
   });
 });
